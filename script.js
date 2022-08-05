cd43e2d514aeaa0878f66eb86ca7e461ccb02e5c
Rithesh Kulkarni <krkrithesh@gmail.com>
2022-08-05 16:10:12 +0530

    Added functionality

---
+++ script.js
@@ -0,0 +1,74 @@
+var arr= [];
+var ans= [];
+
+var but=document.getElementsByTagName("button");
+
+//Enter
+but[0].addEventListener("click", () => {
+    collectInput();
+    arr=[];
+});
+
+//Clear
+but[1].addEventListener("click",() => {
+    clearInput();
+    arr=[];
+});
+
+//Collecting input from boxes
+function collectInput(){
+    var inputs=document.getElementsByClassName("box");
+    for(var i=0;i<inputs.length;i++)
+    {
+        arr.push(parseInt(inputs[i].value));
+    }
+    processingLoad(arr);
+    ans=[];
+}
+
+//the main logic
+function processingLoad(arr){
+    var cnt= new Array(13).fill(0);
+    for(var i of arr)
+    {
+        if(isNaN(i))
+            continue;
+        cnt[i]++;
+    }
+    var p1=12;
+    while(p1>=0){
+        if(cnt[p1-1]==0)
+            multiplePush(ans,p1,cnt[p1]);
+        else if(cnt[p1]>cnt[p1-1])
+            multiplePush(ans,p1,cnt[p1]-cnt[p1-1]);
+        p1--;
+    }
+    ans.sort(function(a,b){return a-b});
+    console.log(ans);
+    addToDOM(ans);
+}
+
+//Adding answer to webpage
+function addToDOM(ans){
+    var html="";
+    for(var i=0;i<ans.length;i++)
+    {
+        html+=ans[i];
+    }
+    document.getElementById("answer").innerHTML=html;
+}
+
+//helper for pushing
+function multiplePush(ans,p1,times){
+    while(times-->0)
+        ans.push(p1);
+}
+
+//main logic for clearing
+function clearInput(){
+    var inputs=document.getElementsByClassName("box");
+    for(var i=0;i<inputs.length;i++)
+        inputs[i].value="";
+    var cl=document.getElementById("answer");
+    cl.replaceChildren();
+}
